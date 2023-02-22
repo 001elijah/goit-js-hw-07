d0fc057cba7984cf14ce2fb920a2284d3f8f1fe4
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-02-22 20:53:04 +0200

    hw 7 solution update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divElement = document.querySelector('div.gallery');
+
+function createGalleryItems(itemsArr) {
+    itemsArr.forEach(obj => {
+        divElement.innerHTML += `<div class="gallery__item">
+        <a class="gallery__link" href="${obj.original}">
+          <img
+            class="gallery__image"
+            src="${obj.preview}"
+            data-source="${obj.original}"
+            alt="${obj.description}"
+          />
+        </a>
+      </div>`
+    });
+};
+
+function selectImage(evt) {
+    evt.preventDefault();
+    if (evt.target.parentNode !== document.body) {
+        basicLightbox.create(`
+		    <img width="1400" height="900" src="${evt.target.dataset.source}">
+	    `).show()
+    };
+};
+
+createGalleryItems(galleryItems);
+
+divElement.addEventListener('click', selectImage);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divElement = document.querySelector('div.gallery');
+
+function createGalleryItems(itemsArr) {
+    itemsArr.forEach(obj => {
+        divElement.innerHTML += `
+        <a class="gallery__link" href="${obj.original}" data-lightbox="lbox">
+          <img
+            class="gallery__image"
+            src="${obj.preview}"
+            data-source="${obj.original}"
+            alt="${obj.description}"
+          />
+        </a>`
+    });
+};
+
+function selectImage(evt) {
+    evt.preventDefault();
+};
+
+createGalleryItems(galleryItems);
+
+divElement.addEventListener('click', selectImage);
+
+$('.gallery a').simpleLightbox({ /* options */ });
